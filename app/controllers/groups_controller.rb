3f95d80e538e24b7516e1a2c9624b047f5238cd2
superzrr <superzrr@gmail.com>
2017-04-12 13:51:51 +0800

    add validation to tile apply to create/update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,9 +9,12 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
-    @group.save
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
 
-    redirect_to groups_path
   end
 
   def show
@@ -24,15 +27,17 @@ class GroupsController < ApplicationController
 
   def update
     @group = Group.find(params[:id])
-    @group.update(group_params)
-
-    redirect_to groups_path, notice: "Update Success"
+    if @group.update(group_params)
+      redirect_to groups_path, notice: "Update Success"
+    else
+      render :edit
+    end
   end
 
   def destroy
     @group = Group.find(params[:id])
-    @group.destroy
     redirect_to groups_path, alert:"Delete Success"
+      
   end
 
   private
