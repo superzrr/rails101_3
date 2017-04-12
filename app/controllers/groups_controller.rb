acdce55dbdd2d06a69ba99d2aa88bd94c18851ac
superzrr <superzrr@gmail.com>
2017-04-12 17:03:00 +0800

    install devise

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user!, only:[:new]
+
   def index
     @groups = Group.all
   end
@@ -37,7 +39,7 @@ class GroupsController < ApplicationController
   def destroy
     @group = Group.find(params[:id])
     redirect_to groups_path, alert:"Delete Success"
-      
+
   end
 
   private
